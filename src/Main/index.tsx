2afcea48aeadc5d73bd94cb30a4f9d49d3707543
Thiago Ramos <trs.tiba@gmail.com>
2023-01-04 19:51:04 -0300

    Configurações e componente Header

---
+++ src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header';
+import { Container } from './styles';
+
+export function Main() {
+  return(
+    <Container>
+      <Header />
+    </Container>
+  );
+}
