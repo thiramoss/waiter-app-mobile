6d7045749cb03a2b512a8f55d70f55ca0bc4305e
Thiago Ramos <trs.tiba@gmail.com>
2023-01-05 09:57:27 -0300

    Componente button

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
@@ -26,7 +27,11 @@ export function Main() {
       </Container>
 
       <Footer>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <Button onPress={() => alert('Clicou')}>
+            Novo Pedido
+          </Button>
+        </FooterContainer>
       </Footer>
     </>
   );
--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -20,6 +20,9 @@ export const MenuContainer = styled.View`
 export const Footer = styled.View`
   min-height: 110px;
   background: #FFF;
+  padding: 16px 24px;
 `;
 
-export const FooterContainer = styled.SafeAreaView``;
+export const FooterContainer = styled.SafeAreaView`
+
+`;
