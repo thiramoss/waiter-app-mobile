7987d4915c1c4989e8b7a943322a14e92cb3226c
Thiago Ramos <trs.tiba@gmail.com>
2023-01-05 19:09:36 -0300

    Componente menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,40 @@
+import { FlatList } from 'react-native';
+import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={product => product._id}
+      ItemSeparatorComponent={Separator}
+      renderItem={({ item: product }) => (
+        <>
+          <Product>
+            <ProductImage
+              source={{
+                uri: `http://192.168.15.151:3001/uploads/${product.imagePath}`,
+              }}
+            />
+            <ProductDetails>
+              <Text weight='600'>{product.name}</Text>
+              <Text size={14} color="#666" style={{ marginVertical: 8}}>{product.description}</Text>
+              <Text>{formatCurrency(product.price)}</Text>
+            </ProductDetails>
+
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </Product>
+        </>
+      ) }
+
+    />
+
+  );
 }
