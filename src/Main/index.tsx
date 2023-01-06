827392ad38dfcf429814063c4a390ce5a2b370cc
Thiago Ramos <trs.tiba@gmail.com>
2023-01-06 12:00:35 -0300

    Componente Modal

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 import {
   Container,
   CategoriesContainer,
@@ -11,6 +13,14 @@ import {
 } from './styles';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
+
   return(
     <>
       <Container>
@@ -28,11 +38,20 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Clicou')}>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
             Novo Pedido
-          </Button>
+            </Button>
+          )}
+
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
